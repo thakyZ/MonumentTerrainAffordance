9fe6762b6245d89314bf9ce785acf3ce02dba6f0
thakyZ <thakyz@outlook.com>
2020-07-10 12:21:39 -0700

    Added build script

---
+++ dist.bat
@@ -0,0 +1,16 @@
+@echo off
+
+powershell -NoProfile -Command "Remove-Item -Recurse -Path .\dist"
+powershell -NoProfile -Command "New-Item -ItemType Directory -Path .\dist"
+powershell -NoProfile -Command "New-Item -ItemType Directory -Path \".\dist\MonumentTerrainAffordance\""
+powershell -NoProfile -Command "New-Item -ItemType Directory -Path \".\dist\MonumentTerrainAffordance\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\About\About.xml \".\dist\MonumentTerrainAffordance\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\About\Manifest.xml \".\dist\MonumentTerrainAffordance\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\Assemblies \".\dist\MonumentTerrainAffordance\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\Source \".\dist\MonumentTerrainAffordance\""
+powershell -NoProfile -Command "Remove-Item -Recurse -Path \".\dist\MonumentTerrainAffordance\Source\obj\""
+powershell -NoProfile -Command "Remove-Item -Recurse -Path \".\dist\MonumentTerrainAffordance\Source\Connected Services\""
+powershell -NoProfile -Command "Remove-Item -Path \".\dist\MonumentTerrainAffordance\Source\MonumentTerrainAffordance.csproj.user\""
+powershell -NoProfile -Command "Get-ChildItem \".\dist\MonumentTerrainAffordance\Source\" -Recurse -Hidden | Remove-Item -Recurse -Verbose -Force"
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\LICENSE \".\dist\MonumentTerrainAffordance\""
+powershell -NoProfile -Command "Copy-Item -Recurse -Path .\Sarcophagus_Patch.sln \".\dist\MonumentTerrainAffordance\""
